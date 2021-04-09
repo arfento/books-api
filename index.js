862b51d5dc2feaf6b93f0032fddba37490b2d011
arfento <arfento@gmail.com>
2021-04-10 01:18:48 +0700

    add CRUD

--- index.js
+++ index.js
@@ -12,7 +12,52 @@ const books = [
 
 
 app.get('/', (req, resp) =>{
-    resp.send('Welccome to Learn API with node JS')
+    resp.send('Welcome to Learn API with node JS!!')
+});
+
+app.get('/api/books', (req, res) => {
+    res.send(books);
+});
+
+app.get('/api/books/:id' , (req, res) =>{
+    const book = books.find( v => v.id === parseInt(req.params.id));
+    if(!book){
+        res.status(404).send('Books not Found');
+    }
+    res.send(book);
+});
+
+
+app.post('/api/books/addBook', (req, res) =>{
+    const book = {
+        id: books.length+1,
+        title : req.body.title,
+    };
+    books.push(book);
+    res.send(book);
+});
+
+app.put('/api/books/:id' , (req, res) =>{
+    const book = books.find( v => v.id === parseInt(req.params.id));
+    if(!book){
+        res.status(404).send('Books not Found');
+    }
+
+    book.title = req.body.title;
+    
+    res.send(book);
+});
+
+app.delete('/api/books/:id' , (req, res) =>{
+    const book = books.find( v => v.id === parseInt(req.params.id));
+    if(!book){
+        res.status(404).send('Books not Found');
+    }
+    const index = books.indexOf(book);
+    books.splice(index, 1);
+    res.send(book);
+
+    console.log("data berhasil terhapus");
 });
 
 const port = 3000;
